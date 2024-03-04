b640ad12779150dd5dedd3f98e9511922249e357
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-03-03 22:38:48 -0300

    add editor action

--- app/routes/editor.$id/route.tsx
+++ app/routes/editor.$id/route.tsx
@@ -1,15 +1,33 @@
-import { ensureAuthenticated } from "@/action/middlewares";
+import * as yup from "yup";
+import { ensureAuthenticated, ensureBody } from "@/action/middlewares";
 import { FormType } from "@/form/types";
 import { prisma } from "@/lib/prisma";
-import { LoaderFunctionArgs, json, redirect } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  json,
+  redirect,
+} from "@remix-run/node";
+import { useLoaderData, useSubmit } from "@remix-run/react";
 import { EditorProvider } from "./editor/provider";
 import { NavbarEditor } from "./editor/components/navbar-editor";
 import { EditorSection } from "./editor/components/editor-section";
 
 export default function Page() {
+  const submit = useSubmit();
   const { form } = useLoaderData<typeof loader>();
-  async function handleSubmit(data: FormType) {}
+  async function handleSubmit(data: FormType) {
+    console.log(data);
+
+    submit(
+      {
+        name: data.name,
+        screens: JSON.stringify(data.screens),
+        endScreen: JSON.stringify(data.endScreen),
+      },
+      { method: "POST" }
+    );
+  }
 
   return (
     <EditorProvider handleSubmit={handleSubmit} form={form as any}>
@@ -34,4 +52,30 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
   if (!form) return redirect("/dashboard");
 
   return json({ form });
+}
+
+const updateFormSchema = yup.object();
+
+type updateFormType = yup.InferType<typeof updateFormSchema>;
+
+export async function action({ request, params }: ActionFunctionArgs) {
+  const body = (await ensureBody<updateFormType>(
+    updateFormSchema,
+    request
+  )) as any;
+  const { id } = await ensureAuthenticated(request);
+
+  await prisma.form.update({
+    where: {
+      id: params.id,
+      customerId: id,
+    },
+    data: {
+      name: body.name,
+      screens: JSON.parse(body.screens),
+      endScreen: JSON.parse(body.endScreen),
+    },
+  });
+
+  return json({});
 }
